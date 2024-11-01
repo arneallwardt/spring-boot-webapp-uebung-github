1a2668aed156336762cd03be2dbdada97d3ff924
arneallwardt <132394779+arneallwardt@users.noreply.github.com>
2024-11-01 12:16:14 +0100

    added test stuff

---
+++ src/test/java/com/example/demo/DemoApplicationIT.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+public class DemoApplicationIT {
+
+    @Test
+    void my_simple_integration_test() {
+        System.out.println("This is a integration test!");
+    }
+
+    @Test
+    void my_simple_integration_test2() {
+        System.out.println("This is a second integration test!");
+    }
+}
